fd7c702dbc165d127794ede2f1c8813773c922e5
Behnam Yousefi <46751569+behnam-yousefi@users.noreply.github.com>
2023-07-29 13:06:13 +0200

    mnda

--- Usage_Examples/network_generation_ex.R
+++ Usage_Examples/network_generation_ex.R
@@ -7,7 +7,7 @@ myNet = network_gen(n.nodes = 100, n.var.nodes = 5, n.var.nei = 90, noise.sd = .
 graph_data = myNet$data_graph
 var_nodes = myNet$var_nodes
 
-embeddingSpaceList = plexi_embedding_2layer(graph_data, train.rep = 50)
+embeddingSpaceList = plexi_embedding_2layer(graph_data, train.rep = 5)
 plexi_output = plexi_node_detection_2layer(embeddingSpaceList)
 
 print(plexi_output$high_var_nodes_index)
