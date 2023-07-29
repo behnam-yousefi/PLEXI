44d3e424a7d027bcd643c689e568e631ecb55a9f
Behnam Yousefi <46751569+behnam-yousefi@users.noreply.github.com>
2023-07-29 15:16:46 +0200

    Usage exampels OK

--- Usage_Examples/network_generation_ex.R
+++ Usage_Examples/network_generation_ex.R
@@ -7,8 +7,12 @@ myNet = network_gen(n.nodes = 100, n.var.nodes = 5, n.var.nei = 90, noise.sd = .
 graph_data = myNet$data_graph
 var_nodes = myNet$var_nodes
 
-embeddingSpaceList = plexi_embedding_2layer(graph_data, train.rep = 5)
-plexi_output = plexi_node_detection_2layer(embeddingSpaceList)
+embeddingSpaceList = plexi_embedding_2layer(graph_data, train.rep = 50, 
+                                            random.walk = FALSE, null.perm = FALSE)
+plexi_output = plexi_node_detection_2layer(embeddingSpaceList, p.adjust.method = "none")
 
-print(plexi_output$high_var_nodes_index)
-print(var_nodes)
+myNet$var_nodes
+plexi_output$significant_nodes_index
+plexi_output$high_ranked_nodes_index
+plexi_output$high_var_nodes_index
+hist(plexi_output$p_values)
